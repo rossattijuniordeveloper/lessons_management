ad7b297a865e394c4b3de2fbeb6a1c51e0d5db08
Mario Rossatti Junior <rossattijuniordeveloper@gmail.com>
2024-07-04 01:29:25 -0300

    ajuste nos imports via STS.

--- app/src/main/java/com/tecnopar/app/api/teachers/controllers/TeacherRestController.java
+++ app/src/main/java/com/tecnopar/app/api/teachers/controllers/TeacherRestController.java
@@ -2,22 +2,24 @@ package com.tecnopar.app.api.teachers.controllers;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tecnopar.app.api.common.routes.ApiRoutes;
 import com.tecnopar.app.api.teachers.dtos.TeacherResponse;
 import com.tecnopar.app.api.teachers.services.TeacherService;
 
 import lombok.RequiredArgsConstructor;
-import main.java.com.tecnopar.app.api.common.routes.ApiRoutes;
+
 
 @RestController
 @RequiredArgsConstructor
 public class TeacherRestController {
     
-    
+    @Autowired
     private final TeacherService teacherService;
 
     @GetMapping(ApiRoutes.LIST_TEACHERS)
